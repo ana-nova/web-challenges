64e51c4ce0c963c8df3c05d8aeb89981a90e3c6b
ana-nova <yu.ana@gmx.de>
2024-09-19 10:42:46 +0200

    add loops: for loop

---
+++ js-loops_for-console/index.js
@@ -0,0 +1,14 @@
+console.clear();
+
+/* Allgemeiner Aufbau einer For-Schleife:
+for (initialisierung; bedingung; aktualisierung) {
+    // Code, der ausgeführt wird, solange die Bedingung erfüllt ist
+  }
+
+*/
+
+  //um 0 bis 100 zu zählen:
+  for (let i = 0; i <= 100; i++) {
+    console.log(i);
+  }
+  
