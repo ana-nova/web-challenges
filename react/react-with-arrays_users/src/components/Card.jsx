6a4759356f9a702715fb5d53105554f48b9cc086
ana-nova <yu.ana@gmx.de>
2024-10-02 16:54:23 +0200

    add comment

--- react/react-with-arrays_users/src/components/Card.jsx
+++ react/react-with-arrays_users/src/components/Card.jsx
@@ -7,7 +7,11 @@ function Card({ user }) {
     <article className="card">
       <h2>{user.name}</h2>
       <ul className="card__taglist">
-        <Tag tag={user.roles[0]} />
+
+        {user.roles.map((role, index) => (
+          <Tag key={index} tag={role} />
+        ))}
+
       </ul>
       <p>{user.about}</p>
       <Button>edit</Button>
@@ -20,5 +24,13 @@ export default Card;
 
 
 
-// "user" dient hier als prob und greift auf die relevanten Informationen (wie name, roles, about) zu, die aus der App-Komponente kommen 
+// "user" dient hier als prob und greift auf die relevanten Informationen 
+//(wie name, roles, about) zu, die aus der App-Komponente kommen 
 
+/*
+
+Warum index? line 11
+Du verwendest index als key, weil die einzelnen Rollen keinen eindeutigen Bezeichner haben. 
+Der index stellt sicher, dass React eine eindeutige Kennung für jedes Element in der Liste hat.
+
+*/
