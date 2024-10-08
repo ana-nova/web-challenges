12c8884bcf3a11d811266bf4122e02b7b8925535
ana-nova <yu.ana@gmx.de>
2024-10-08 13:05:39 +0200

    update

--- js/js-unit-testing_get-animal/index.js
+++ js/js-unit-testing_get-animal/index.js
@@ -17,6 +17,6 @@ export function getAnimal(animal) {
 /*
     was wird erwartet?
         1. wenn ich "cats" übergebe, dann erwarte ich "i totally love cats"
-        2. bei !cats: "i like dogs" bzw alles andere als cats
+        2. bei   : "i like dogs" bzw alles andere als cats
         3. wenn kein argument übergeben word, dann "i dont like animals at all"
 */
