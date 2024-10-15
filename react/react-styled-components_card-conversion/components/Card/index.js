00d770f18ac9ef2e219af14ad003cb46ba3db603
ana-nova <yu.ana@gmx.de>
2024-10-15 16:09:14 +0200

    add styled-components by reducing .css file

---
+++ react/react-styled-components_card-conversion/components/Card/index.js
@@ -0,0 +1,55 @@
+import styled from "styled-components";
+
+export default function Card() {
+  return (
+    <Body>
+      <Title>Homer Simpson</Title>
+      <Paragraph>
+        Homer Jay Simpson is a fictional character and the main protagonist of
+        the American animated sitcom The Simpsons. He is voiced by Dan
+        Castellaneta and first appeared, along with the rest of his family, in
+        The Tracey Ullman Show short Good Night on April 19, 1987.
+      </Paragraph>
+    </Body>
+  );
+}
+
+const Body = styled.div`
+  border-radius: 4px;
+  padding: 8px;
+  box-shadow: 0px 2px 1px -1px rgba(0, 0, 0, 0.2),
+    0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 1px 3px 0px rgba(0, 0, 0, 0.12);
+`;
+
+const Title = styled.h3`
+  margin-top: 0;
+  margin-bottom: 12px;
+  font-size: 1.25rem;
+`;
+
+const Paragraph = styled.p`
+  margin-top: 0;
+  margin-bottom: 4px;
+`;
+
+/*
+Card.css looked like that before deleting:
+.card {
+  border-radius: 4px;
+  padding: 8px;
+  box-shadow: 0px 2px 1px -1px rgba(0, 0, 0, 0.2),
+    0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 1px 3px 0px rgba(0, 0, 0, 0.12);
+}
+
+.card__title {
+  margin-top: 0;
+  margin-bottom: 12px;
+  font-size: 1.25rem;
+}
+
+.card__description {
+  margin-top: 0;
+  margin-bottom: 4px;
+}
+
+*/
