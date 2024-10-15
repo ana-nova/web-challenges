74cc39d11772a09e6a1f7cfbc613e457d89f9b9c
ana-nova <yu.ana@gmx.de>
2024-10-15 12:20:07 +0200

    add random button

--- nextJS/lotr-app-dynamic-routes/pages/volumes/index.js
+++ nextJS/lotr-app-dynamic-routes/pages/volumes/index.js
@@ -3,6 +3,17 @@ import { volumes, introduction } from "../../lib/data";
 import { useRouter } from "next/router";
 
 export default function Volumes() {
+  function getRandomElement(array) {
+    return volumes[Math.floor(Math.random() * volumes.length)];
+  }
+
+  const router = useRouter();
+
+  function handleRandom(volumes) {
+    const randomVolume = getRandomElement(volumes);
+    router.push(`/volumes/${randomVolume.slug}`);
+  }
+
   return (
     <>
       <h1>The Lord of the Rings</h1>
@@ -14,6 +25,9 @@ export default function Volumes() {
           </li>
         ))}
       </ul>
+      <button type="button" onClick={handleRandom}>
+        random click
+      </button>
     </>
   );
 }
