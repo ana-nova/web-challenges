85ff49797368979281f188ef15f0bbcc40711a10
ana-nova <yu.ana@gmx.de>
2024-10-17 17:22:16 +0200

    include styling and animation lottie

--- nextJS/lotr-app-dynamic-routes/pages/index.js
+++ nextJS/lotr-app-dynamic-routes/pages/index.js
@@ -1,9 +1,38 @@
+import Image from "next/image";
 import Link from "next/link";
+import { DotLottieReact } from "@lottiefiles/dotlottie-react";
 
 export default function HomePage() {
   return (
     <div>
-      <Link href="/volumes">Go to all volumes</Link>
+      <h1>Lord of the Rings 💍</h1>
+      <div style={{ display: "flex", gap: "20px", margin: "20px" }}>
+        <Image
+          src="/images/the-fellowship-of-the-ring.png"
+          height={230}
+          width={144}
+          alt="lotr pic #1"
+        />
+        <Image
+          src="/images/the-two-towers.png"
+          height={230}
+          width={144}
+          alt="lotr pic #2"
+        />
+        <Image
+          src="/images/the-return-of-the-king.png"
+          height={230}
+          width={144}
+          alt="lotr pic #3"
+        />
+      </div>
+      <Link href="/volumes">Volumes Overview 🎬</Link>
+      <DotLottieReact
+        src="/animation/movie.lottie"
+        loop
+        autoplay
+        style={{ width: 300, height: 300 }}
+      />
     </div>
   );
 }
--- nextJS/lotr-app-dynamic-routes/pages/volumes/index.js
+++ nextJS/lotr-app-dynamic-routes/pages/volumes/index.js
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import { volumes, introduction } from "../../lib/data";
 import { useRouter } from "next/router";
+import styled from "styled-components";
 
 export default function Volumes() {
   function getRandomElement(array) {
@@ -15,19 +16,75 @@ export default function Volumes() {
   }
 
   return (
-    <>
-      <h1>The Lord of the Rings</h1>
-      <p>{introduction}</p>
-      <ul>
-        {volumes.map((volume) => (
-          <li key={volume.slug}>
-            <Link href={`/volumes/${volume.slug}`}>{volume.title}</Link>
-          </li>
-        ))}
-      </ul>
-      <button type="button" onClick={handleRandom}>
-        random click
-      </button>
-    </>
+    <div style={styles.container}>
+      <Card>
+        <h1 style={styles.title}>The Lord of the Rings</h1>
+        <p>{introduction}</p>
+        <ul style={styles.list}>
+          {volumes.map((volume) => (
+            <li key={volume.slug} style={styles.listItem}>
+              <StyledLink href={`/volumes/${volume.slug}`}>
+                {volume.title}
+              </StyledLink>
+            </li>
+          ))}
+        </ul>
+        <button
+          type="button"
+          onClick={() => handleRandom(volumes)}
+          style={styles.button}
+        >
+          show randomly
+        </button>
+      </Card>
+    </div>
   );
 }
+
+// ---------------------- styles:
+const StyledLink = styled(Link)`
+  text-decoration: none;
+`;
+
+const Card = styled.div`
+  background-color: #fff;
+  padding: 20px;
+  border-radius: 10px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  text-align: center;
+  max-width: 400px;
+  width: 100%;
+  transition: transform 0.3s ease, box-shadow 0.3s ease;
+
+  &:hover {
+    transform: scale(1.05);
+    box-shadow: 0 8px 16px rgba(0, 0, 0, 0.2);
+  }
+`;
+
+const styles = {
+  container: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    minHeight: "100vh",
+    backgroundColor: "#f0f0f0",
+  },
+
+  list: {
+    listStyleType: "none",
+    padding: 0,
+    marginBottom: "20px",
+  },
+  listItem: {
+    marginBottom: "10px",
+  },
+  button: {
+    padding: "10px 20px",
+    backgroundColor: "#ec3375",
+    color: "#fff",
+    border: "none",
+    borderRadius: "5px",
+    cursor: "pointer",
+  },
+};
